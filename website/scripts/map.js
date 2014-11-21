382c86e7e79e66b361a411c1f4d4431dd3744f0b
Tuyet Trang Thi Pham <tuyettrang@dhcp-030223.wlan.ntnu.no>
2014-11-21 11:00:49 +0100

    rydder litt

--- website/scripts/map.js
+++ website/scripts/map.js
@@ -15,26 +15,3 @@ function initialize() {
             });
         }
         google.maps.event.addDomListener(window, 'load', initialize);
-		
-		var slideimages=new Array()
-		var slidelinks=new Array()
-		function slideshowimages(){
-		
-		for (i=0;i<slideshowimages.arguments.length;i++){
-			slideimages[i]=new Image()
-			slideimages[i].src=slideshowimages.arguments[i]
-			}
-		}
-
-		function slideshowlinks(){
-		for (i=0;i<slideshowlinks.arguments.length;i++)
-		slidelinks[i]=slideshowlinks.arguments[i]
-		}
-
-		function gotoshow(){
-		if (!window.winslide||winslide.closed)
-		winslide=window.open(slidelinks[whichlink])
-		else
-		winslide.location=slidelinks[whichlink]
-		winslide.focus()
-		}
