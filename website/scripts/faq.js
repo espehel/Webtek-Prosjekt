524a44b1984ff7d9249b5bd8b1ca746592f00161
Tuyet Trang Thi Pham <tuyettrang@dhcp-030155.wlan.ntnu.no>
2014-11-24 16:06:37 +0100

    tab fix for all sites

--- website/scripts/faq.js
+++ website/scripts/faq.js
@@ -4,31 +4,31 @@ window.addEventListener("load", function(event){
 	var faqList = document.getElementById('faqList');
 
 	if (window.XMLHttpRequest) {// for IE7+, Firefox, Chrome, Opera, Safari
-  		xmlhttp=new XMLHttpRequest();
-  	}
+		xmlhttp=new XMLHttpRequest();
+	}
 	else {// for IE6, IE5
-  		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  	}
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
 
-xmlhttp.open("GET","data/faq.xml",false);
-xmlhttp.send();
-xmlDoc=xmlhttp.responseXML; 
+	xmlhttp.open("GET","data/faq.xml",false);
+	xmlhttp.send();
+	xmlDoc=xmlhttp.responseXML;
 
-var x=xmlDoc.getElementsByTagName("item");
-console.log(x);
-for (i=0; i<x.length; i++)
-  { 
-  	var li = document.createElement("li");
-  	var question = document.createElement("p");
-  	question.appendChild(document.createTextNode(x[i].getElementsByTagName("question")[0].childNodes[0].nodeValue));
-  	var answer = document.createElement("p");
-  	answer.appendChild(document.createTextNode(x[i].getElementsByTagName("answer")[0].childNodes[0].nodeValue));
-  	li.appendChild(getHeader("Question:"))
-    	li.appendChild(question);
-  	li.appendChild(getHeader("Answer:"))
-    	li.appendChild(answer);
-    	faqList.appendChild(li);
-  }
+	var x=xmlDoc.getElementsByTagName("item");
+	console.log(x);
+	for (i=0; i<x.length; i++)
+	{
+		var li = document.createElement("li");
+		var question = document.createElement("p");
+		question.appendChild(document.createTextNode(x[i].getElementsByTagName("question")[0].childNodes[0].nodeValue));
+		var answer = document.createElement("p");
+		answer.appendChild(document.createTextNode(x[i].getElementsByTagName("answer")[0].childNodes[0].nodeValue));
+		li.appendChild(getHeader("Question:"))
+		li.appendChild(question);
+		li.appendChild(getHeader("Answer:"))
+		li.appendChild(answer);
+		faqList.appendChild(li);
+	}
 
 });
 
--- website/scripts/map.js
+++ website/scripts/map.js
@@ -1,17 +1,17 @@
 function initialize() {
-            var myLatlng = new google.maps.LatLng(63.430515,10.395053);
-            var mapOptions = {
-                center: myLatlng,
-                zoom: 16
-            };
-            var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var myLatlng = new google.maps.LatLng(63.430515,10.395053);
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 16
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-            // Place a draggable marker on the map
-            var marker = new google.maps.Marker({
-                position: myLatlng,
-                map: map,
-                draggable:true,
-                title:"Drag me!"
-            });
-        }
-        google.maps.event.addDomListener(window, 'load', initialize);
+    // Place a draggable marker on the map
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        draggable:true,
+        title:"Drag me!"
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
